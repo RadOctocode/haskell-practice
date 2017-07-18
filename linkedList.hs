a4f46241dd6788fb74deba5658a890d337538bbd
RadOctocode <happylife345@gmail.com>
2017-07-18 00:16:04 -0400

    added addNode and removeNode

--- linkedList.hs
+++ linkedList.hs
@@ -4,19 +4,29 @@ data Node a = Cons a (Node a) | Null deriving Show --PREPEND TO A LIST (let y = 
 myHead :: Node a -> a
 myHead Null = error "its empty"
 myHead x = case x of 
-	                (Cons a (_)) -> a
+	                Cons a (_) -> a
 
 myTail:: Node a -> a
 myTail Null = error "its empty"
 myTail x = case x of 
-					(Cons a (Null)) -> a
-					(Cons a (b)) -> myTail b
+					Cons a (Null) -> a
+					Cons a (b) -> myTail b
+
 
 addNode:: Node a -> Node a -> Node a 
---go through the list similar to myTail and add the node
--- add to end
---x is node to add y is list to add too 
 addNode Null y = error "you cant add a empty value"
 addNode x Null = x 
-addNode x y = case x y of
-						
+addNode x y = case y of
+					Cons a Null -> Cons a x 
+					Cons a b -> Cons a(addNode x b)
+
+removeNode:: Eq a => a -> Node a -> Node a
+removeNode x y = case y of
+					Null -> Null
+					Cons a (Null) 
+						|a == x -> Null
+						|otherwise -> Cons a (Null)
+					Cons a b
+						|a == x -> b
+						|otherwise -> Cons a (removeNode x b)
+
