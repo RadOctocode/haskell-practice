9f0b605b75fd34340361faee8314ed9e4fe12dca
RadOctocode <happylife345@gmail.com>
2017-07-24 23:40:41 -0400

    added binarytree and style fixes for linkedList

---
+++ binary.hs
@@ -0,0 +1,13 @@
+data Node a = Cons a (Node a) (Node a)| Null deriving (Show, Ord, Eq) --PREPEND TO A LIST (let y = Cons 1 Null) or (let x = Null)
+emptyBin:: Node a -> Bool
+emptyBin Null = True
+emptyBin x = False
+
+insert :: Ord a => a -> Node a -> Node a
+insert a Null = Cons a (Null) (Null)
+insert a b = case b of
+					Cons x y z 
+						| a > x -> Cons x (y) (insert a z)
+						| a < x -> Cons x (insert a y) (z)
+						| a == x -> error "you cant put in the same value"
+
--- linkedList.hs
+++ linkedList.hs
@@ -1,36 +1,31 @@
 
-data Node a = Cons a (Node a) | Null deriving Show --PREPEND TO A LIST (let y = Cons 1 Null) or (let x = Null)
+--PREPEND TO A LIST (let y = Cons 1 Null) or (let x = Null)
+data Node a = Cons a (Node a) | Null deriving Show 
 
 myHead :: Node a -> a
 myHead Null = error "its empty"
-myHead x = case x of 
-	                Cons a (_) -> a
+myHead (Cons a _) = a
 
-myTail:: Node a -> a
+myTail :: Node a -> a
 myTail Null = error "its empty"
-myTail x = case x of 
-					Cons a (Null) -> a
-					Cons a (b) -> myTail b
+myTail (Cons a Null) = a
+myTail (Cons a b) = myTail b
 
-
-addNode:: Node a -> Node a -> Node a 
+addNode :: Node a -> Node a -> Node a 
 addNode Null y = error "you cant add a empty value"
 addNode x Null = x 
-addNode x y = case y of
-					Cons a Null -> Cons a x 
-					Cons a b -> Cons a(addNode x b)
+addNode x (Cons a Null) = Cons a x 
+addNode x (Cons a b) = Cons a(addNode x b)
 
-removeNode:: Eq a => a -> Node a -> Node a
-removeNode x y = case y of
-					Null -> Null
-					Cons a (Null) 
-						|a == x -> Null
-						|otherwise -> Cons a (Null)
-					Cons a b
-						|a == x -> b
-						|otherwise -> Cons a (removeNode x b)
+removeNode :: Eq a => a -> Node a -> Node a
+removeNode x Null = Null
+removeNode x (Cons a (Null))
+	|a == x = Null
+	|otherwise = Cons a (Null)
+removeNode x (Cons a b)
+	|a == x = b
+	|otherwise = Cons a (removeNode x b)
 
-addNodeHead::Eq a => a -> Node a -> Node a
+addNodeHead :: Eq a => a -> Node a -> Node a
 addNodeHead x Null = Cons x Null
-addNodeHead x y = case y of 
-					Cons a b -> Cons x (addNodeHead a b)
+addNodeHead x y = Cons x (y)
